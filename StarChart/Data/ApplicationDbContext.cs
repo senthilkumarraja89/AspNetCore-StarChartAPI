1cd26af29ecd6107f31f00d5c7e52de83a6d18c8
senthilkumarraja89 <60222389+senthilkumarraja89@users.noreply.github.com>
2020-01-23 19:00:51 +0530

    Update ApplicationDbContext.cs

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObject { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
